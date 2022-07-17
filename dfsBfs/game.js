32010275c6628029b8cbf19b29ea344fc067c9b4
Yuyu <ujunglim@naver.com>
2022-07-17 20:13:54 +0800

    game

--- dfsBfs/game.js
+++ dfsBfs/game.js
@@ -1,39 +1,46 @@
 const directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]; // up right down left
 
 function solution(maps) {
-  const queue = [[0,0,1]];  // init [x,y,step]
-  const targetX = maps[0].length-1;
-  const targetY = maps.length-1;
+    const queue = [{x: 0, y: 0, step: 1}];
+    const targetX = maps.length-1;
+    const targetY = maps[0].length-1;
 
-  while(queue.length !== 0) {
-    const current = queue.shift();
-    const [x, y, step] = current;
-    // console.log(x, y, step);
-    // arrived
-    if(x === targetX && y === targetY) return step;
 
-    // find adj
-    for(let i = 0; i < 4; i++) {
-      const [dx, dy] = directions[i];
-      const adjX = x+dx;
-      const adjY = y+dy;
-      // out of map
-      if(adjX < 0 || adjY < 0 || adjX > targetX || adjY > targetY) continue;
-      // found route
-      if(maps[adjX][adjY] === 1) {
-        maps[adjX][adjY] = 0;
-        // console.log('found:', [adjX, adjY]);
-        queue.push([adjX, adjY, step+1]);
-      }
+    const isMovable = (x, y) => x >=0 && x <= targetX && y >=0 && x <= targetY && maps[x][y] === 1;
+
+
+    while(queue.length !== 0) {
+        const current = queue.shift();
+        const {x, y, step} = current;
+        // console.log(x, y, step);
+        // arrived
+        if(x === targetX && y === targetY) return step;
+
+        // up
+        if(isMovable(x-1, y)) {
+            queue.push({x: x-1, y: y, step: step+1});
+            maps[x][y] = 0;
+        }
+        // right
+        if(isMovable(x, y+1)) {
+            queue.push({x: x, y: y+1, step: step+1});
+            maps[x][y] = 0;
+        }
+        // down
+        if(isMovable(x+1, y)) {
+            queue.push({x: x+1, y: y, step: step+1});
+            maps[x][y] = 0;
+        }
+        // left
+        if(isMovable(x, y-1)) {
+            queue.push({x: x, y: y-1, step: step+1});
+            maps[x][y] = 0;
+        }
     }
-  }
-  return -1;
+    return -1;
 }
 
-// const input = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]];
-const input = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]];
+const input = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]];
+// const input = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]];
 const result = solution(input);
 console.log(result);
-
-// continue
-// index계산이랑 adj계산 동일하게
--- dfsBfs/wordChangeCount.js
+++ dfsBfs/wordChangeCount.js
@@ -1,38 +1,70 @@
-const diffChar = (word1, word2) => {
-  const length = word1.length;
+function isNext (curr, next) {
   let diff = 0;
-  for (let i = 0; i < length; i++) {
-    if (word1[i] !== word2[i]) {
-      diff++;
-    }
+  for(let i = 0; i < curr.length; i++) {
+      if(curr[i] !== next[i]) diff++;
   }
-  return diff;
-};
+  return (diff === 1) ? true : false;
+}
 
 function solution(begin, target, words) {
   // init
-  let queue = [{current: begin, level: 0}];
+  const queue = [{current: begin, step: 0}];
 
   while(queue.length !== 0) {
-    const {current, level} = queue.shift();
-    let newWords = []; // remain words
-
-    if(current === target) return level;
-    
-    // find adjacent
-    for(let i = 0; i < words.length; i++) {
-      const diff = diffChar(current, words[i]);
-      if(diff === 1) {
-        queue.push({current: words[i], level: level + 1});
+      const {current, step} = queue.shift();
+      const remains = [];
+      // found target
+      if(current === target) return step;
+      // filter nexts and remains
+      for(let i = words.length-1; i >= 0; i--) {
+          if(isNext(current, words[i])) {
+              queue.push({current: words[i], step: step+1});
+          }
+          else {
+              remains.push(words[i]);
+          }
       }
-      else {
-        newWords.push(words[i]);
-      }
-    }
-    words = newWords;
+      words = remains;
   }
   return 0;
+  
 }
+
+// const diffChar = (word1, word2) => {
+//   const length = word1.length;
+//   let diff = 0;
+//   for (let i = 0; i < length; i++) {
+//     if (word1[i] !== word2[i]) {
+//       diff++;
+//     }
+//   }
+//   return diff;
+// };
+
+// function solution(begin, target, words) {
+//   // init
+//   let queue = [{current: begin, level: 0}];
+
+//   while(queue.length !== 0) {
+//     const {current, level} = queue.shift();
+//     let newWords = []; // remain words
+
+//     if(current === target) return level;
+    
+//     // find adjacent
+//     for(let i = 0; i < words.length; i++) {
+//       const diff = diffChar(current, words[i]);
+//       if(diff === 1) {
+//         queue.push({current: words[i], level: level + 1});
+//       }
+//       else {
+//         newWords.push(words[i]);
+//       }
+//     }
+//     words = newWords;
+//   }
+//   return 0;
+// }
 
 const begin = "hit";
 const target = "cog"; //cog
